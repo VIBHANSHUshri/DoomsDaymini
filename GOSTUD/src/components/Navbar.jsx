b3a12c3408cc7a7691d56f49c466d965e49d5b3d
Arpita Jain <arpitajain0628@gmail.com>
2024-03-01 23:27:56 +0530

    navbar updation

--- GOSTUD/src/components/Navbar.jsx
+++ GOSTUD/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
         <div className='bg-gradient-to-r from-gray-100 via-white to-gray-100 border-t-4 border-gray-200 rounded-t-md p-2 '>
           <div className='flex justify-evenly items-center'>
-            <div className='flex gap-6'>
+            <div className='flex gap-6 hover:cursor-pointer'>
             <svg width="18" height="18" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24ZM26.5016 38.1115V25.0542H30.1059L30.5836 20.5546H26.5016L26.5077 18.3025C26.5077 17.1289 26.6192 16.5001 28.3048 16.5001H30.5581V12H26.9532C22.6231 12 21.0991 14.1828 21.0991 17.8536V20.5551H18.4V25.0547H21.0991V38.1115H26.5016Z" fill="black"/>
             </svg>
@@ -30,7 +30,7 @@ const Navbar = () => {
                 GO STUD
             </div>
 
-            <div className='border border-black solid text-xs px-3 py-0.5 font-bold'>
+            <div className='border border-black solid text-xs px-3 py-0.5 font-bold hover:cursor-pointer'>
               WORK WITH US
             </div>
             </div>
